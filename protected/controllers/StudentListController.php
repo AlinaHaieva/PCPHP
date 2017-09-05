ee6ae43ea1be8b8e9c4b8ac54e677606a3305527
AlinaHaieva <alina.gaevaya@gmail.com>
2017-09-05 15:49:31 +0300

    add studentListController + component

--- protected/controllers/StudentListController.php
+++ protected/controllers/StudentListController.php
@@ -2,9 +2,24 @@
 
 class StudentListController extends BaseController
 {
-    public function actionGetStudentList()
+    public function actionEnglishTable($group_id)
     {
-        $students = StudentList::model()->getStudentList();
-        $this->renderJSON($students);
+        /** @var StudentComponent $component */
+        $rawData = Yii::app()->getComponent('Student')->getStudentList($group_id);
+        $arrayDataProvider = new CArrayDataProvider($rawData, array(
+            'id' => 'id',
+            'sort' => array(
+                'attributes' => array(
+                    'Name', 'English level',
+                ),
+            ),
+            'pagination' => array(
+                'pageSize' => 5,
+            ),
+        ));
+        $params = array(
+            'arrayDataProvider' => $arrayDataProvider,
+        );
+        $this->renderPartial('../site/studentList', $params);
     }
 }
